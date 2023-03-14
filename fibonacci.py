a72697bf4236e3a51deabd94b5c4a8b515c4e7b8
Colin Charles <colincharles1204@gmail.com>
2023-03-15 00:02:04 +0530

    Factorial

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,11 @@
 def fibonacci(n):
-    if n == 0:
+    if n == 1:
         return 0
-    elif n == 1:
+    elif n == 2:
         return 1
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
 n = int(input('Enter number : '))
-for i in range(n):
-    print(fibonacci(i), end=' ')
+print(fibonacci(n), end=' ')
